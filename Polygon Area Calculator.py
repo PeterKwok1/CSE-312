4dd3fbca3009c207b98e65d001d39cf6ace70176
Peter Kwok <peter.kwok94@gmail.com>
2024-06-02 23:02:29 -0400

    feat: outline feat: basic rect methods

--- Polygon Area Calculator.py
+++ Polygon Area Calculator.py
@@ -1,6 +1,39 @@
 class Rectangle:
-    pass
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+
+    def __str__(self) -> str:
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def set_width(self, new_width):
+        self.width = new_width
+
+    def set_height(self, new_height):
+        self.height = new_height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width**2 + self.height**2) ** 0.5  # hypotenuse formula
+
+    def get_picture(self):
+        rect_str = ""
+        for line in range(self.height):
+            for space in range(self.width):
+                pass
+
+
+a = Rectangle(3, 4)
+print(a.get_diagonal())
 
 
 class Square:
     pass
+
+
+# how many squares in rectangle without rotating: square side must be less than rect height and width to fit, then integer division height and width and multiply.
--- README.md
+++ README.md
@@ -1,11 +1,47 @@
 
 # Polygon Area Calculator 
 
-Description
+Rectangle class with set_width, set_height, get_area, get_perimeter, get_diagonal, get_picture, and get_amount_inside methods. 
+
+Square subclass with additional set_side method. 
 
 Example
 ```
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
 
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+print(rect.get_amount_inside(sq))
+
+<!-- Returns -->
+50
+26
+Rectangle(width=10, height=3)
+**********
+**********
+**********
+
+81
+5.656854249492381
+Square(side=4)
+****
+****
+****
+****
+
+8
 ```
 
 For the purpose of reviewing python to follow cse 312. 
