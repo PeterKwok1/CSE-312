771d7c108e72235ca523bffbe50d330b3af5dd73
Peter Kwok <peter.kwok94@gmail.com>
2024-06-04 18:47:14 -0400

    feat: dict to list, testing

--- Probability Calculator.py
+++ Probability Calculator.py
@@ -3,7 +3,12 @@ import random
 
 
 class Hat:
-    pass
+    def __init__(self, **kwargs) -> None:
+        # dict to list
+        self.contents = []
+        for ball, number in kwargs.items():  # similar to enumerate or a list of lists.
+            for _ in range(number):
+                self.contents.append(ball)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
@@ -12,3 +17,12 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
 # copy.deepcopy(x)
 # list to dict
+# list to unique list: https://www.geeksforgeeks.org/python-get-unique-values-list/
+# .dict() or increment dict value over list
+
+hat_test = Hat(red=1, green=2, blue=3)
+print(hat_test.contents)
+
+test = [[1, 2, 3], [4, 5, 6]]
+for a, b, c in test:
+    print(a, b, c)
