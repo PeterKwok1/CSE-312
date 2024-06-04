41a48d12e8855e23273e6109b5ad6f4ee51c14cc
Peter Kwok <peter.kwok94@gmail.com>
2024-06-04 17:47:21 -0400

    feat: outline

--- Probability Calculator.py
+++ Probability Calculator.py
@@ -1,59 +1,14 @@
-class Rectangle:
-    def __init__(self, width, height) -> None:
-        self.width = width
-        self.height = height
-
-    def __str__(self) -> str:
-        return f"Rectangle(width={self.width}, height={self.height})"
-
-    def set_width(self, new_width):
-        self.width = new_width
-
-    def set_height(self, new_height):
-        self.height = new_height
-
-    def get_area(self):
-        return self.width * self.height
-
-    def get_perimeter(self):
-        return 2 * self.width + 2 * self.height
-
-    def get_diagonal(self):
-        return (self.width**2 + self.height**2) ** 0.5  # hypotenuse formula
-
-    def get_picture(self):
-        if self.width > 50 or self.height > 50:
-            return "Too big for picture."
-        rect_str = ""
-        for line in range(self.height):
-            for space in range(self.width):
-                rect_str += "*"
-            rect_str += "\n"
-        return rect_str
-
-    def get_amount_inside(self, shape):
-        # square side must be less than rect width and height to fit. integer division height and width and multiply.
-        width_fit = self.width // shape.width
-        height_fit = self.height // shape.height
-        return width_fit * height_fit
+import copy
+import random
 
 
-# inheritance: https://www.w3schools.com/python/python_inheritance.asp
-class Square(Rectangle):
-    def __init__(self, side) -> None:
-        super().__init__(side, side)
+class Hat:
+    pass
 
-    def __str__(self) -> str:
-        return f"Square(side={self.width})"
 
-    def set_side(self, side):
-        self.width = side
-        self.height = side
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
 
-    def set_width(self, side):
-        self.width = side
-        self.height = side
 
-    def set_height(self, side):
-        self.width = side
-        self.height = side
+# copy.deepcopy(x)
+# list to dict
--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 
 # Probability Calculator 
 
-Description
+Hat class:
+Takes a variable number of arguments each representing the number of balls of a color.
+Draw method which removes random balls from the hat. 
+
+experiment():
+Takes a hat object, expected_balls object indicating the group of balls to attempt to draw from the hat, num_balls_drawn indicating the number of balls to draw each experiment, num_experiments indicating the number of experiements to perform. 
+Returns an approximante probability of drawing the expected group of balls from the hat based on a number of experiments.
 
 Example
 ```
+hat = Hat(black=6, red=4, green=3)
+probability = experiment(hat=hat,
+                  expected_balls={"red":2,"green":1},
+                  num_balls_drawn=5,
+                  num_experiments=2000)
 
+<!-- Returns -->
+0.356
 ```
 
 For the purpose of reviewing python to follow cse 312. 
