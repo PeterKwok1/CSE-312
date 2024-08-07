e54a16d24a5a4cfbbe3a5cf52da7c3f8ad85c3a0
Peter Kwok <peter.kwok94@gmail.com>
2024-08-07 19:44:58 -0400

    git test

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
 import socketserver
 from routes import routes
 
+from util.router import Router
+from util.request import Request
+
+app = Router
+
+app.add_route()
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
 
+        # parse request
+        received_data = self.request.recv(2048)
+        print(self.client_address)
+        print("--- received data ---")
+        print(received_data)
+        print("--- end of data ---\n\n")
+        request = Request(received_data)
+
+        # route request
+        app.route_request(request)
         # construct request and response
-        routes(self)
+        # routes(self)
+        # test
 
         # TODO: Parse the HTTP request and use self.request.sendall(response) to send your response
 
